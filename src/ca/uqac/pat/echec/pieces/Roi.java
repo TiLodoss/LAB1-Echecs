b091d8e48853333687b3db7baa3d7f4e24838ed3
Vincent <vincent.aunai@gmail.com>
2014-09-25 11:15:49 -0400

    Petit roque en trichant testé et fonctionnel

--- src/ca/uqac/pat/echec/pieces/Roi.java
+++ src/ca/uqac/pat/echec/pieces/Roi.java
@@ -26,9 +26,50 @@ public class Roi extends Piece implements Cloneable{
   * @return Vrai si le déplacement a eu lieu correctement, faux sinon.
   */
 	public boolean Bouger (int Joueur, int incX, int incY, boolean PourVrai){
-		if (Math.abs(incX) + Math.abs (incY) <= 2 )
+		// DEBUT MODIF : tentative de roque - Petit roque
+		if (Math.abs(incX) == 2 && Math.abs(incY) == 0) {
+			// On vérifie que le roi n'a pas déjà bougé
+			if (!dejaBouge) return petitRoqueEnTrichant(Joueur, incX, incY, PourVrai);
+			else return false;
+		}
+				
+		else if (Math.abs(incX) + Math.abs (incY) <= 2 ) {
+			dejaBouge = true;
 			return super.Bouger(Joueur, incX, incY, PourVrai);
+		}
+		
+		
+		
 		else return false;
+	}
+	
+	// AJOUT : méthode pour le petit roque en trichant
+	private boolean petitRoqueEnTrichant(int Joueur, int incX, int incY, boolean PourVrai) {
+		// Vérification des deux cases à droite du roi : si elles sont vides...
+		if ((Jeu.Case(this.PosX+1, this.PosY).isCaseVide()) && 
+			(Jeu.Case(this.PosX+2, this.PosY).isCaseVide())) {
+			//... on autorise le petit roque : déplacement du roi
+			if (super.Bouger(Joueur, incX, incY, PourVrai)) {
+				System.out.println("Le roi s'est déplacé");
+				// déplacement de la tour
+				if (Joueur == 1) {
+					Jeu.setPos(7, 0, new CaseVide(Jeu, 7, 0));
+					Jeu.setPos(5, 0, new Tour(Jeu, Joueur, 5, 0));
+					
+					System.out.println("Tour blanche déplacée");
+				}
+				
+				else {
+					Jeu.setPos(7, 7, new CaseVide(Jeu, 7, 7));
+					Jeu.setPos(5,7, new Tour(Jeu, Joueur, 5, 7));
+					
+					System.out.println("Tour noire déplacée");
+				}
+			}
+			
+			return true;
+		}
+		return false;
 	}
 
 	
